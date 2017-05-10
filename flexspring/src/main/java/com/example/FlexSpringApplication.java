b5514d4c7a8d8e7f810b4b0b2fd4ea81b3be65e9
Bill <bill.puhl@gmail.com>
2017-05-10 09:42:52 -0400

    initial commit

---
+++ flexspring/src/main/java/com/example/FlexSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexSpringApplication.class, args);
+	}
+}
